2de045847cf3917f78b3dba80d1b52bf26602010
Ardneran007 <narendrarraut@gmail.com>
2022-06-17 01:09:01 +0530

    Endpoint for deleting a Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -46,7 +46,7 @@ router.post('/addnote',fetchuser,[
     }
 })
 
-//ROUTE 3:Update a existing Note using :POST "/api/notes/updatenote" Login required
+//ROUTE 3:Update a existing Note using :PUT "/api/notes/updatenote" Login required
 router.put('/updatenote/:id',fetchuser,async(req,res)=>{
 
     const {title,description,tag}=req.body;
@@ -74,4 +74,33 @@ router.put('/updatenote/:id',fetchuser,async(req,res)=>{
 
 })
 
+
+//ROUTE 4:Delete a existing Note using :DELETE "/api/notes/deletenote" Login required
+router.delete('/deletenote/:id',fetchuser,async(req,res)=>{
+
+    const {title,description,tag}=req.body;
+    try{
+        //Create a new Note object
+        const newNote={};
+        if(title){newNote.title = title}
+        if(description){newNote.description = description}
+        if(tag){newNote.tag = tag}
+
+        //Find a Note to be deleted and delete it
+        let note=await Note.findById(req.params.id);
+        if(!note){return res.status(404).send("Not found")}
+        
+        //Allow deletion only if user owns this note
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("Not allowed");
+        }
+
+        note=await Note.findByIdAndDelete(req.params.id)
+        res.json({"success":"Note has been deleted"});
+    }catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
+})
 module.exports=router
