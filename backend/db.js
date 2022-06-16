aacb96d8b2a446c199576fa502041527143cff1c
Ardneran007 <narendrarraut@gmail.com>
2022-06-16 11:25:55 +0530

    Creating ThunderClient Collections to Manage Requests

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb://localhost:27017/ibook"
+const mongoURI="mongodb://localhost:27017/iNotebook"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,27 +2,41 @@ const express=require('express');
 const User = require('../models/User');
 const router =express.Router();
 const { body, validationResult } = require('express-validator');
-//Creaye a User using :POST
 
-router.post("/",[
+//Create a User using :POST "/api/auth/createuser"
+router.post("/createuser",[
     body('name','Enter a valid name').isLength({min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','PAssword must be atleast 5 characters').isLength({min:5}),
 
-],(req,res)=>{
+], async(req,res)=>{
+    //if there are errors bad req will be return and errors will also be returned
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    //check whether user with same email exists already
+    try {
+      
+    
+    let user=await User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({error:"Sorry a User with this email already exists"})
+    }
+    //create a new User
+    user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=>console.log(err)
-      );
+      })
+      
+      res.send(user);
+      //Catch errors
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
 
-    //   res.send(req.body);
 })
 
 module.exports=router
